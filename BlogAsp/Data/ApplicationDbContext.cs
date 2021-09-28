63c5b7b11e7b50b64675c4cf631eb0cd9be594db
StounhandJ <rmarinicev@gmail.com>
2021-09-28 10:12:11 +0300

    first

---
+++ BlogAsp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogAsp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAsp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+         public DbSet<User> Users { get; set; }
+         
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
